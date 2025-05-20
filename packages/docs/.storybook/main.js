4b3be89e6de64215282626902b3d6196710f3485
Rafael Mattei <rafaelrmattei@gmail.com>
2025-05-20 11:03:19 -0300

    Initial commit

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,11 @@
+export default {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+}
